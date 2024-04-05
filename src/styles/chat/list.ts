e3e79fc8a2043b3cbdb888435a6bab6418a7f657
dhapdhap123 <dhapdhap123@naver.com>
2024-04-05 18:43:16 +0900

    채팅 리스트 api 추가 & 채팅 룸 데이터 형식 변경 & 초과 텍스트 수정 로직

--- src/styles/chat/list.ts
+++ src/styles/chat/list.ts
@@ -1,132 +1,132 @@
-import styled from 'styled-components';
-import { Link } from 'react-router-dom';
+import styled from "styled-components";
+import { Link } from "react-router-dom";
 
 ////////NoChatsComponent////////
 export const NoChatsContainer = styled.div`
-    width: 100vw;
-    height: calc(100vh - 20rem);
-    display: flex;
-    flex-direction: column;
-    align-items: center;
-    justify-content: center;
+  width: 100vw;
+  height: calc(100vh - 20rem);
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
 `;
 
 export const NoChatsText = styled.div`
-    font-size: 0.8rem;
-    font-weight: 500;
-    margin-top: 1rem;
-    color: #acacac;
+  font-size: 0.8rem;
+  font-weight: 500;
+  margin-top: 1rem;
+  color: #acacac;
 `;
 
 export const NoChatsLink = styled(Link)`
-    font-size: 1rem;
-    font-weight: 700;
-    margin-top: 1rem;
-    color: black;
-    text-decoration: underline;
+  font-size: 1rem;
+  font-weight: 700;
+  margin-top: 1rem;
+  color: black;
+  text-decoration: underline;
 `;
 
 ////////ChatListComponent////////
 
 export const ChatContainer = styled.div`
-    width: 100vw;
-    height: 5rem;
-    display: flex;
-    align-items: center;
-    border-bottom: 1px solid #ebebeb;
-    background: white;
-    padding: 0 5vw;
+  width: 100vw;
+  height: 5rem;
+  display: flex;
+  align-items: center;
+  border-bottom: 1px solid #ebebeb;
+  background: white;
+  padding: 0 5vw;
 `;
 
 export const ChatImgWrapper = styled.div`
-    width: 15vw;
-    display: flex;
-    justify-content: flex-start;
-    align-items: center;
+  width: 15vw;
+  display: flex;
+  justify-content: flex-start;
+  align-items: center;
 `;
 
 export const ChatImg = styled.img`
-    width: 2.5rem;
-    height: 2.5rem;
-    border-radius: 50%;
+  width: 2.5rem;
+  height: 2.5rem;
+  border-radius: 50%;
 `;
 
 export const ChatContent = styled.div`
-    width: 60vw;
-    display: flex;
-    flex-direction: column;
-    justify-content: center;
-    align-items: flex-start;
-    padding: 0.3rem 0 0;
+  width: 60vw;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: flex-start;
+  padding: 0.3rem 0 0;
 `;
 
 export const ChatName = styled.div`
-    width: 60vw;
-    height: 1.1rem;
-    font-size: 0.9rem;
-    font-weight: 700;
+  width: 60vw;
+  height: 1.1rem;
+  font-size: 0.9rem;
+  font-weight: 700;
 `;
 
 export const ChatText = styled.div`
-    width: 60vw;
-    height: 1.2rem;
-    margin-top: 0.2rem;
-    font-size: 0.8rem;
-    color: #7e7e7e;
-    text-overflow: ellipsis; // 텍스트가 너무 길면 ...으로 표시
-    white-space: nowrap;
-    overflow: hidden;
+  width: 60vw;
+  height: 1.2rem;
+  margin-top: 0.2rem;
+  font-size: 0.8rem;
+  color: #7e7e7e;
+  text-overflow: ellipsis; // 텍스트가 너무 길면 ...으로 표시
+  white-space: nowrap;
+  overflow: hidden;
 `;
 
 export const ChatSubContent = styled.div`
-    width: 15vw;
-    display: flex;
-    flex-direction: column;
-    justify-content: center;
-    align-items: flex-end;
+  width: 15vw;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: flex-end;
 `;
 
 export const ChatTime = styled.div`
-    font-size: 0.7rem;
-    color: #7e7e7e;
-    margin-bottom: 0.5rem;
+  font-size: 0.7rem;
+  color: #7e7e7e;
+  margin-bottom: 0.5rem;
 `;
 
 export const ChatNum = styled.div`
-    width: 1.1rem;
-    height: 1.1rem;
-    border-radius: 50%;
-    display: flex;
-    justify-content: center;
-    align-items: center;
-    background-color: #6d2fef;
-    color: white;
-    font-size: 0.7rem;
-    text-align: center;
+  width: 1.3rem;
+  height: 1.3rem;
+  border-radius: 50%;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  background-color: #6d2fef;
+  color: white;
+  font-size: 0.7rem;
+  text-align: center;
 `;
 
 ///////ChatList//////////
 export const Container = styled.div`
-    width: 100vw;
-    height: 100vh;
-    display: flex;
-    flex-direction: column;
-    justify-content: flex-start;
-    align-items: center;
-    background: #f2f3f6;
-    overflow: scroll;
+  width: 100vw;
+  height: 100vh;
+  display: flex;
+  flex-direction: column;
+  justify-content: flex-start;
+  align-items: center;
+  background: #f2f3f6;
+  overflow: scroll;
 `;
 
 export const TItleContainer = styled.div`
-    width: 90vw;
-    height: 4rem;
-    display: flex;
-    justify-content: flex-start;
-    align-items: flex-end;
+  width: 90vw;
+  height: 4rem;
+  display: flex;
+  justify-content: flex-start;
+  align-items: flex-end;
 `;
 
 export const Title = styled.div`
-    font-size: 1.5rem;
-    font-weight: 700;
-    margin: 0.8rem 0;
+  font-size: 1.5rem;
+  font-weight: 700;
+  margin: 0.8rem 0;
 `;
