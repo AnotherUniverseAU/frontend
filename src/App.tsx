5ab30c5d1e8e89297faca0563e9ef7d2aa04ae4e
dhapdhap123 <dhapdhap123@naver.com>
2024-04-22 21:59:04 +0900

    contain test

--- src/App.tsx
+++ src/App.tsx
@@ -22,7 +22,7 @@ export const App = () => {
         <title>AU</title>
         <meta
           name="viewport"
-          content="width=device-width, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0, user-scalable=no, viewport-fit=cover"
+          content="width=device-width, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0, user-scalable=no, viewport-fit=contain"
         ></meta>
       </Helmet>
       <GlobalStyle />
