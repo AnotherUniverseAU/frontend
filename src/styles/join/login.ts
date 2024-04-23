30258703e93b96d7c3e8771dd52b781212770e0d
dhapdhap123 <dhapdhap123@naver.com>
2024-04-23 19:02:29 +0900

    order change test

--- src/styles/join/login.ts
+++ src/styles/join/login.ts
@@ -2,7 +2,8 @@ import styled from "styled-components";
 
 export const Container = styled.div`
   width: 100%;
-  height: 100vh;
+  height: 100%;
+  padding: 5rem;
   display: flex;
   flex-direction: column;
   align-items: center;
