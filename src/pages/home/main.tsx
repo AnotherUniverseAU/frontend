ac98ecca765564093da19bc6afbcdc60db22182d
cucumber5252 <kangdonghyuk@gangdonghyeog-ui-noteubug.local>
2024-04-27 21:04:17 +0900

    REFACT: Main Loading

--- src/pages/home/main.tsx
+++ src/pages/home/main.tsx
@@ -29,6 +29,8 @@ export const Main: React.FC = () => {
     const [filteredCharacters, setFilteredCharacters] = useState<any[]>([]);
     const [selectedGenre, setSelectedGenre] = useState<string>('all');
 
+    const [loading, setLoading] = useState<boolean>(true);
+
     useEffect(() => {
         const fetchCharacters = async () => {
             const result = await apiRequestGet('/character/list');
@@ -37,6 +39,7 @@ export const Main: React.FC = () => {
 
         const fetchMainCharacter = async () => {
             const result = await apiRequestGet('/character/main-character');
+            setLoading(false);
             setMainCharacter(result.mainCharacter);
         };
 
@@ -55,66 +58,81 @@ export const Main: React.FC = () => {
     }, [characters, selectedGenre]);
 
     return (
-        <S.Container style={{ overflow: isTutorial ? 'hidden' : 'scroll' }}>
-            {/* {isTutorial && (
-                <S.TutorialContainer>
-                    <S.TutorialTextContainer>
-                        <S.TutorialText>원하는 AI 캐릭터를</S.TutorialText>
-                        <S.TutorialText>직접 만들 수 있어요!</S.TutorialText>
-                        <S.TutorialButton onClick={() => setIsTutorial(false)}>OK</S.TutorialButton>
-                    </S.TutorialTextContainer>
-                </S.TutorialContainer>
+        <>
+            {!loading ? (
+                <S.Container style={{ overflow: isTutorial ? 'hidden' : 'scroll' }}>
+                    {isTutorial && (
+                        <S.TutorialContainer className="header">
+                            <S.TutorialTextContainer>
+                                <S.TutorialText>원하는 AI 캐릭터를</S.TutorialText>
+                                <S.TutorialText>직접 만들 수 있어요!</S.TutorialText>
+                                <S.TutorialButton onClick={() => setIsTutorial(false)}>OK</S.TutorialButton>
+                            </S.TutorialTextContainer>
+                        </S.TutorialContainer>
+                    )}
+                    {isTutorial && (
+                        <S.VirtualHeader className="header">
+                            <S.VirtualLeft />
+                            <S.VirtualRight>
+                                <S.CreateImg src={createWhiteImg} alt="createImg" />
+                                <S.CircleImg src={circleWhiteImg} alt="circle White Img" />
+                            </S.VirtualRight>
+                        </S.VirtualHeader>
+                    )}
+                    <MainHeader toCreate={true} isTutorial={isTutorial} />
+                    <S.SubContainer className="container">
+                        <S.MainContainer>
+                            <MainImage
+                                route={`/detail/${mainCharacter?.characterId}`}
+                                imgurl={mainCharacter.mainImageUrl}
+                                name={mainCharacter.name}
+                                title={mainCharacter.title}
+                                creatorNickname={mainCharacter.creatorNickname}
+                            />
+                        </S.MainContainer>
+                        <S.NavContainer>
+                            <S.ButtonNav>
+                                <S.StyledButton
+                                    active={selectedGenre === 'all'}
+                                    onClick={() => setSelectedGenre('all')}
+                                >
+                                    전체
+                                </S.StyledButton>
+                                <S.StyledButton
+                                    active={selectedGenre === 'anime'}
+                                    onClick={() => setSelectedGenre('anime')}
+                                >
+                                    애니
+                                </S.StyledButton>
+                                <S.StyledButton
+                                    active={selectedGenre === 'game'}
+                                    onClick={() => setSelectedGenre('game')}
+                                >
+                                    게임
+                                </S.StyledButton>
+                            </S.ButtonNav>
+                            <S.ImageContainer>
+                                {filteredCharacters.map((character: any, index) => (
+                                    <S.StyledImageMargin key={character.characterId}>
+                                        <StyledImage
+                                            route={`/detail/${character.characterId}`}
+                                            imgurl={character.coverImageUrl}
+                                            name={character.name}
+                                            title={character.title}
+                                            creatorNickname={character.creatorNickname}
+                                        />
+                                    </S.StyledImageMargin>
+                                ))}
+                            </S.ImageContainer>
+                        </S.NavContainer>
+                    </S.SubContainer>
+                    <IconFooter activepage="/" />
+                    <BackgroundUpper color="white" />
+                    <BackgroundLower color="#f2f3f6" />
+                </S.Container>
+            ) : (
+                <Loading />
             )}
-            {isTutorial && (
-                <S.VirtualHeader>
-                    <S.VirtualLeft />
-                    <S.VirtualRight>
-                        <S.CreateImg src={createWhiteImg} alt="createImg" />
-                        <S.CircleImg src={circleWhiteImg} alt="circle White Img" />
-                    </S.VirtualRight>
-                </S.VirtualHeader>
-            )} */}
-            <MainHeader toCreate={true} isTutorial={isTutorial} />
-            <S.SubContainer className="container">
-                <S.MainContainer>
-                    <MainImage
-                        route={`/detail/${mainCharacter?.characterId}`}
-                        imgurl={mainCharacter.mainImageUrl}
-                        name={mainCharacter.name}
-                        title={mainCharacter.title}
-                        creatorNickname={mainCharacter.creatorNickname}
-                    />
-                </S.MainContainer>
-                <S.NavContainer>
-                    <S.ButtonNav>
-                        <S.StyledButton active={selectedGenre === 'all'} onClick={() => setSelectedGenre('all')}>
-                            전체
-                        </S.StyledButton>
-                        <S.StyledButton active={selectedGenre === 'anime'} onClick={() => setSelectedGenre('anime')}>
-                            애니
-                        </S.StyledButton>
-                        <S.StyledButton active={selectedGenre === 'game'} onClick={() => setSelectedGenre('game')}>
-                            게임
-                        </S.StyledButton>
-                    </S.ButtonNav>
-                    <S.ImageContainer>
-                        {filteredCharacters.map((character: any, index) => (
-                            <S.StyledImageMargin key={character.characterId}>
-                                <StyledImage
-                                    route={`/detail/${character.characterId}`}
-                                    imgurl={character.coverImageUrl}
-                                    name={character.name}
-                                    title={character.title}
-                                    creatorNickname={character.creatorNickname}
-                                />
-                            </S.StyledImageMargin>
-                        ))}
-                    </S.ImageContainer>
-                </S.NavContainer>
-            </S.SubContainer>
-            <IconFooter activepage="/" />
-            <BackgroundUpper color="white" />
-            <BackgroundLower color="#f2f3f6" />
-        </S.Container>
+        </>
     );
 };
