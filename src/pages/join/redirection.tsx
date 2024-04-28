55b09bfe437600ef1a5ce2cfc8e4cd31c211c423
cucumber5252 <kangdonghyuk@gangdonghyeog-ui-noteubug.local>
2024-04-28 23:18:46 +0900

    REFACT: Main Route Error test

--- src/pages/join/redirection.tsx
+++ src/pages/join/redirection.tsx
@@ -1,56 +1,43 @@
-import { useNavigate } from "react-router";
-import { useEffect, useState } from "react";
-import axios from "axios";
-import { Loading } from "../setting/loading";
+import { useNavigate } from 'react-router';
+import { useEffect, useState } from 'react';
+import axios from 'axios';
+import { Loading } from '../setting/loading';
 
 export const Redirection = () => {
-  const params = new URLSearchParams(window.location.search);
-  const [isLogin, setIsLogin] = useState(false);
+    const params = new URLSearchParams(window.location.search);
+    const [isLogin, setIsLogin] = useState(false);
 
-  const navigate = useNavigate();
+    const navigate = useNavigate();
 
-  useEffect(() => {
-    const getToken = async () => {
-      const code = params.get("code");
+    useEffect(() => {
+        const getToken = async () => {
+            const code = params.get('code');
 
-      const res = await axios.post(
-        `${process.env.REACT_APP_BASE_URL}/oauth/kakao`,
-        {
-          code: code,
-        }
-      );
-      const { access_token, refresh_token } = res.data;
-      return { access_token, refresh_token };
-    };
+            const res = await axios.post(`${process.env.REACT_APP_BASE_URL}/oauth/kakao`, {
+                code: code,
+            });
+            const { access_token, refresh_token } = res.data;
+            return { access_token, refresh_token };
+        };
 
-    getToken()
-      .then((res) => {
-        localStorage.setItem("accessToken", res.access_token);
-        localStorage.setItem("refreshToken", res.refresh_token);
-        return res.refresh_token;
-      })
-      .then((res) => {
-        if ((window as any).ReactNativeWebView) {
-          (window as any).ReactNativeWebView.postMessage(
-            JSON.stringify({
-              type: "STORE_REFRESH_TOKEN",
-              token: res,
+        getToken()
+            .then((res) => {
+                localStorage.setItem('accessToken', res.access_token);
+                localStorage.setItem('refreshToken', res.refresh_token);
+                return res.refresh_token;
             })
-          );
+            .then(() => {
+                setIsLogin(true);
+            });
+    }, []);
+
+    useEffect(() => {
+        if (isLogin === true) {
+            navigate('/nickname');
         }
-      })
-      .then(() => {
-        setIsLogin(true);
-      });
-  }, []);
+    }, [isLogin]);
 
-  useEffect(() => {
-    if (isLogin === true) {
-      navigate("/nickname");
-    }
-  }, [isLogin]);
-
-  return <Loading></Loading>;
+    return <Loading></Loading>;
 };
 
 // import { useEffect } from "react";
