53064e2496d039fcef97a3c2dba26ffe0b8125b0
cucumber5252 <kangdonghyuk@gangdonghyeog-ui-noteubug.local>
2024-05-06 22:01:42 +0900

    REFACT: Logo Img PNG -> SVG

--- src/components/header/main/mainHeader.ts
+++ src/components/header/main/mainHeader.ts
@@ -1,81 +1,81 @@
-import styled from "styled-components";
-import { Link } from "react-router-dom";
+import styled from 'styled-components';
+import { Link } from 'react-router-dom';
 
 export const HeaderContainer = styled.div`
-  width: 100%;
+    width: 100%;
 
-  position: fixed;
-  top: 0;
-  left: 0;
-  z-index: 10;
+    position: fixed;
+    top: 0;
+    left: 0;
+    z-index: 10;
 
-  display: flex;
-  flex-direction: row;
-  align-items: center;
-  justify-content: center;
+    display: flex;
+    flex-direction: row;
+    align-items: center;
+    justify-content: center;
 
-  background-color: white;
+    background-color: white;
 `;
 
 export const StyledLink = styled(Link)`
-  width: 100%;
-  height: 100%;
-  display: flex;
-  flex-direction: row;
-  align-items: center;
-  justify-content: center;
+    width: 100%;
+    height: 100%;
+    display: flex;
+    flex-direction: row;
+    align-items: center;
+    justify-content: center;
 `;
 
 export const CreateContainer = styled.div`
-  position: relative;
-  width: 33.3%;
-  height: 33.3%;
-  z-index: 99;
+    position: relative;
+    width: 33.3%;
+    height: 33.3%;
+    z-index: 99;
 `;
 
 export const ImgLogo = styled.img`
-  width: 43.623%;
-  height: 33.3%;
+    width: 43.623%;
+    height: 33.3%;
 `;
 
-export const TextLogo = styled.img`
-  width: auto;
+export const TextLogo = styled.svg`
+    width: auto;
 `;
 
 export const CreateImg = styled.img`
-  width: 1.5rem;
-  height: 1.5rem;
+    width: 1.5rem;
+    height: 1.5rem;
 `;
 
 export const CircleImg = styled.img`
-  width: 6rem;
-  height: 3rem;
-  position: absolute;
-  top: -0.75rem;
-  right: -0.8rem;
-  z-index: 1000;
+    width: 6rem;
+    height: 3rem;
+    position: absolute;
+    top: -0.75rem;
+    right: -0.8rem;
+    z-index: 1000;
 `;
 
 export const TitleText = styled.div`
-  width: 100%;
-  height: 100%;
-  display: flex;
-  flex-direction: row;
-  align-items: center;
-  justify-content: center;
-  font-size: 1.2rem;
-  font-weight: 900;
+    width: 100%;
+    height: 100%;
+    display: flex;
+    flex-direction: row;
+    align-items: center;
+    justify-content: center;
+    font-size: 1.2rem;
+    font-weight: 900;
 `;
 
 export const LeftDiv = styled.div`
-  width: 20%;
-  height: 100%;
+    width: 20%;
+    height: 100%;
 `;
 export const CenterDiv = styled.div`
-  width: 60%;
-  height: 100%;
+    width: 60%;
+    height: 100%;
 `;
 export const RightDiv = styled.div`
-  width: 20%;
-  height: 100%;
+    width: 20%;
+    height: 100%;
 `;
--- src/components/header/main/mainHeader.tsx
+++ src/components/header/main/mainHeader.tsx
@@ -1,47 +1,33 @@
-import * as S from "./mainHeader.ts";
+import * as S from './mainHeader.ts';
 // import React from "react";
+// MainHeader.tsx
+import React from 'react';
+import { ReactComponent as ImgLogo } from 'src/components/header/main/imgLogo.svg';
+import { ReactComponent as TextLogo } from 'src/components/header/main/textLogo.svg';
+import { ReactComponent as CreatePurpleImg } from 'src/components/header/main/createPurpleImg.svg';
 
-const imgLogo = require("src/components/header/main/imgLogo.png") as string;
-const textLogo = require("src/components/header/main/textLogo.png") as string;
-const createPurpleImg =
-  require("src/components/header/main/createPurpleImg.png") as string;
+export function MainHeader({ toCreate, isTutorial }: { toCreate: boolean; isTutorial: boolean }) {
+    return (
+        <S.HeaderContainer className="header">
+            <S.LeftDiv>
+                <S.StyledLink to={'/'}>
+                    <ImgLogo />
+                </S.StyledLink>
+            </S.LeftDiv>
 
-export function MainHeader({
-  toCreate,
-  isTutorial,
-}: {
-  toCreate: boolean;
-  isTutorial: boolean;
-}) {
-  return (
-    <S.HeaderContainer className="header">
-      <S.LeftDiv>
-        <S.StyledLink to={"/"} style={{ left: 0 }}>
-          <S.ImgLogo src={imgLogo} alt="Img Logo" />
-        </S.StyledLink>
-      </S.LeftDiv>
+            <S.CenterDiv>
+                <S.TitleText>
+                    <TextLogo />
+                </S.TitleText>
+            </S.CenterDiv>
 
-      <S.CenterDiv>
-        <S.TitleText>
-          <S.TextLogo src={textLogo} alt="Text Logo" />
-        </S.TitleText>
-      </S.CenterDiv>
-
-      <S.RightDiv>
-        {toCreate && (
-          <S.StyledLink
-            to={"/createInfo"}
-            style={{ right: 0 }}
-            id="create-char-button"
-          >
-            <S.CreateContainer>
-              {!isTutorial && (
-                <S.CreateImg src={createPurpleImg} alt="createImg" />
-              )}
-            </S.CreateContainer>
-          </S.StyledLink>
-        )}
-      </S.RightDiv>
-    </S.HeaderContainer>
-  );
+            <S.RightDiv>
+                {toCreate && (
+                    <S.StyledLink to={'/createInfo'} id="create-char-button">
+                        <S.CreateContainer>{!isTutorial && <CreatePurpleImg />}</S.CreateContainer>
+                    </S.StyledLink>
+                )}
+            </S.RightDiv>
+        </S.HeaderContainer>
+    );
 }
