55a3c8c76dbe6cb87472009dfdf5bfd1db310240
dhapdhap123 <dhapdhap123@naver.com>
2024-05-14 13:59:02 +0900

    [FEAT] copyright.txt added

---
+++ src/pages/home/copyright.tsx
@@ -0,0 +1,14 @@
+export const Copyright = () => {
+  return (
+    <div style={{ margin: "10px" }}>
+      <p>
+        앱 'AU - 애니 게임 캐릭터와 채팅 (AU_DongHyukKang / com.auandroid)'의
+        소유자와 이 웹사이트의 소유자가 동일하다는 것을 증명합니다.
+      </p>
+      <p style={{ marginTop: "15px" }}>
+        This text evidences that same person(owner) has copyright about app(AU -
+        애니 게임 캐릭터와 채팅/ AU_DongHyukKang / com.auandroid) and this site.
+      </p>
+    </div>
+  );
+};
